ab932fd44dbde28759ff5bdea1819ba911451d05
ramyyyjr <117462665+ramyyyjr@users.noreply.github.com>
2024-11-03 21:56:43 +0100

    Add files via upload
    tp 1 
    Bouzidi Ramy
    Benarbia Imededdine

---
+++ tp1 ai.pl
@@ -0,0 +1,33 @@
+:- dynamic tache/4.
+
+creer_tache(Id, Desc, Assign) :-
+    \+ tache(Id, _, _, _),
+    assertz(tache(Id, Desc, Assign, false)),
+    format("Tâche créée : ~w.~n", [Id]).
+
+assigner_tache(Id, NouveauAssign) :-
+    retract(tache(Id, Desc, _, Statut)),
+    assertz(tache(Id, Desc, NouveauAssign, Statut)),
+    format("Tâche ~w assignée à l’utilisateur : ~w.~n", [Id, NouveauAssign]).
+
+marquer_terminee(Id) :-
+    retract(tache(Id, Desc, Assign, _)),
+    assertz(tache(Id, Desc, Assign, true)),
+    format("Tâche ~w marquée comme terminée.~n", [Id]).
+
+afficher_taches :-
+    forall(tache(Id, Desc, Assign, Statut),
+           (format("Tâche ~w :~n- Description : ~w~n- Assigné : ~w~n- Statut de complétion : ~w~n~n",
+                   [Id, Desc, Assign, Statut]))).
+
+afficher_taches_assignee_a(Assign) :-
+    format("Tâches assignées à ~w :~n", [Assign]),
+    forall(tache(Id, Desc, Assign, Statut),
+           (format("Tâche ~w :~n- Description : ~w~n- Statut de complétion : ~w~n~n",
+                   [Id, Desc, Statut]))).
+
+afficher_taches_terminees :-
+    format("Tâches terminées :~n"),
+    forall(tache(Id, Desc, Assign, true),
+           (format("Tâche ~w :~n- Description : ~w~n- Assigné : ~w~n~n",
+                   [Id, Desc, Assign]))).
